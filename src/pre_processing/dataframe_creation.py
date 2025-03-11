79b049a3d41dd469a5486ea386ac2f153c742c90
arougier <antoine.rougier@ensae.fr>
2025-03-11 12:34:21 +0000

    pre processing of the data creation of a df intermediaire

---
+++ src/pre_processing/dataframe_creation.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+input_pos = 'data/aclImdb/train/pos'
+input_neg = 'data/aclImdb/train/neg'
+
+def load_reviews(directory, label):
+    """Charge les fichiers texte d'un répertoire et les étiquette."""
+    reviews = []
+    if os.path.exists(directory):
+        for filename in os.listdir(directory):
+            if filename.endswith(".txt"):
+                with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
+                    text = file.read()
+                    reviews.append({'text': text, 'label': label})
+    else:
+        print(f"Le répertoire {directory} n'existe pas.")
+    return reviews
+
+pos_reviews = load_reviews(input_pos, 'pos')
+neg_reviews = load_reviews(input_neg, 'neg')
+
+df = pd.DataFrame(pos_reviews + neg_reviews)
+
+output_path = 'data/data_intermediaire.parquet'
+df.to_parquet(output_path, index=False)
+
+print(f"DataFrame sauvegardé en tant que {output_path}")
