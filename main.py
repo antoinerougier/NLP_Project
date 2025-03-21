94e763770b022205b76b9a60b588a780093c8c36
arougier <antoinerougier77@gmail.com>
2025-03-21 17:00:24 +0000

    changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.pre_processing.download_data import download_and_extract_data
 from src.pre_processing.pre_processing import extract_tar
 from src.pre_processing.dataframe_creation import create_dataframe
 from src.model.model import load_data, TextClassificationModel
-from src.visualisation.viz import plot_confusion_matrix, plot_roc_curve, calculate_gini
+from src.visualisation.viz import plot_confusion_matrix, calculate_gini, plot_roc_curve_comparison
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
@@ -28,13 +28,13 @@ def main():
     output_path_test = os.path.join(data_dir, 'data_intermediaire_test.parquet')
 
     # Télécharger et extraire les données
-    download_and_extract_data(url, filename, extract_to)
+    #download_and_extract_data(url, filename, extract_to)
 
     # Extraire le fichier tar (si nécessaire, par exemple si le fichier est déjà extrait)
-    extract_tar(filename, extract_to)
+    #extract_tar(filename, extract_to)
 
     # Créer les DataFrames
-    create_dataframe(input_pos_train, input_neg_train, input_pos_test, input_neg_test, output_path_train, output_path_test)
+    #create_dataframe(input_pos_train, input_neg_train, input_pos_test, input_neg_test, output_path_train, output_path_test)
 
     # Charger les données d'entraînement et de test
     df_train = load_data(output_path_train)
@@ -43,11 +43,9 @@ def main():
     # Définir les paramètres de grille pour Naive Bayes et Logistic Regression
     nb_param_grid = {'alpha': [0.1, 1.0, 10.0, 20.0]}
     svm_param_grid = {
-        'C': [0.1, 1, 10],
-        'kernel': ['linear', 'rbf', 'poly'],
+        'C': [10],
+        'kernel': ['linear'],
         'gamma': ['scale'],
-        'degree': [2, 3],  
-        'coef0': [0.0, 0.1, 0.5]  
     }
     lr_param_grid = {'C': [0.1, 1.0, 10.0], 'solver': ['liblinear', 'lbfgs']}
 
@@ -56,22 +54,39 @@ def main():
     nb_model.train(df_train)
     print("Fin modèle Naive Bayes avec GridSearch")
 
-    svm_model = TextClassificationModel(
+    svm_model_50 = TextClassificationModel(
         model=SVC(probability=True),
         use_svd=True,
         use_grid_search=True,
+        n_components=50,
         param_grid=svm_param_grid
     )
-    svm_model.train(df_train)
-    print("Fin modèle SVM")
+    svm_model_50.train(df_train)
+    print("Fin modèle SVM réduit à 50")
 
-    lr_model = TextClassificationModel(model=LogisticRegression(max_iter=1000), use_svd=True, use_grid_search=True, param_grid=lr_param_grid)
-    lr_model.train(df_train)
-    print("Fin modèle Logistic Regression avec GridSearch")
+    svm_model_100 = TextClassificationModel(
+        model=SVC(probability=True),
+        use_svd=True,
+        use_grid_search=True,
+        n_components=100,
+        param_grid=svm_param_grid
+    )
+    svm_model_100.train(df_train)
+    print("Fin modèle SVM réduit à 100")
+
+    lr_model_50 = TextClassificationModel(model=LogisticRegression(max_iter=1000), use_svd=True, use_grid_search=True, n_components=50, param_grid=lr_param_grid)
+    lr_model_50.train(df_train)
+    print("Fin modèle Logistic Regression avec GridSearch réduit à 50")
+
+    lr_model_100 = TextClassificationModel(model=LogisticRegression(max_iter=1000), use_svd=True, use_grid_search=True, n_components=100, param_grid=lr_param_grid)
+    lr_model_100.train(df_train)
+    print("Fin modèle Logistic Regression avec GridSearch réduit à 100")
     # Évaluer les modèles
     report_nb = nb_model.evaluate(df_test)
-    report_svm = svm_model.evaluate(df_test)
-    report_lr = lr_model.evaluate(df_test)
+    report_svm_50 = svm_model_50.evaluate(df_test)
+    report_lr_50 = lr_model_50.evaluate(df_test)
+    report_svm_100 = svm_model_100.evaluate(df_test)
+    report_lr_100 = lr_model_100.evaluate(df_test)
 
     # Sauvegarder les modèles
     model_dir = "modeles"
@@ -80,14 +95,18 @@ def main():
         print(f"Dossier '{model_dir}' créé.")
 
     dump(nb_model, os.path.join(model_dir, 'nb_model.joblib'))
-    dump(svm_model, os.path.join(model_dir, 'svm_model.joblib'))
-    dump(lr_model, os.path.join(model_dir, 'lr_model.joblib'))
+    dump(svm_model_50, os.path.join(model_dir, 'svm_model_50.joblib'))
+    dump(lr_model_50, os.path.join(model_dir, 'lr_model_50.joblib'))
+    dump(svm_model_100, os.path.join(model_dir, 'svm_model_100.joblib'))
+    dump(lr_model_100, os.path.join(model_dir, 'lr_model_100.joblib'))
 
     # Évaluer les modèles et générer les matrices de confusion
     y_test = df_test['label']
     y_pred_nb = nb_model.best_model.predict(nb_model.vectorizer.transform(df_test['text']))
-    y_pred_svm = svm_model.best_model.predict(svm_model.svd.transform(svm_model.vectorizer.transform(df_test['text'])))
-    y_pred_lr = lr_model.best_model.predict(lr_model.svd.transform(lr_model.vectorizer.transform(df_test['text'])))
+    y_pred_svm_50 = svm_model_50.best_model.predict(svm_model_50.svd.transform(svm_model_50.vectorizer.transform(df_test['text'])))
+    y_pred_lr_50 = lr_model_50.best_model.predict(lr_model_50.svd.transform(lr_model_50.vectorizer.transform(df_test['text'])))
+    y_pred_svm_100 = svm_model_100.best_model.predict(svm_model_100.svd.transform(svm_model_100.vectorizer.transform(df_test['text'])))
+    y_pred_lr_100 = lr_model_100.best_model.predict(lr_model_100.svd.transform(lr_model_100.vectorizer.transform(df_test['text'])))
 
     output_dir = "output"
     if not os.path.exists(output_dir):
@@ -96,36 +115,58 @@ def main():
 
     # Générer et sauvegarder les matrices de confusion
     plot_confusion_matrix(y_test, y_pred_nb, "Confusion Matrix - Naive Bayes", os.path.join(output_dir, 'confusion_matrix_nb.png'))
-    plot_confusion_matrix(y_test, y_pred_svm, "Confusion Matrix - SVM", os.path.join(output_dir, 'confusion_matrix_svm.png'))
-    plot_confusion_matrix(y_test, y_pred_lr, "Confusion Matrix - Logistic Regression", os.path.join(output_dir, 'confusion_matrix_lr.png'))
+    plot_confusion_matrix(y_test, y_pred_svm_50, "Confusion Matrix - SVM", os.path.join(output_dir, 'confusion_matrix_svm_50.png'))
+    plot_confusion_matrix(y_test, y_pred_lr_50, "Confusion Matrix - Logistic Regression", os.path.join(output_dir, 'confusion_matrix_lr_50.png'))
+    plot_confusion_matrix(y_test, y_pred_svm_100, "Confusion Matrix - SVM", os.path.join(output_dir, 'confusion_matrix_svm_100.png'))
+    plot_confusion_matrix(y_test, y_pred_lr_100, "Confusion Matrix - Logistic Regression", os.path.join(output_dir, 'confusion_matrix_lr_100.png'))
 
     # Générer et sauvegarder les courbes ROC AUC
     y_proba_nb = nb_model.best_model.predict_proba(nb_model.vectorizer.transform(df_test['text']))[:, 1]
-    y_proba_svm = svm_model.best_model.predict_proba(svm_model.svd.transform(svm_model.vectorizer.transform(df_test['text'])))[:, 1]
-    y_proba_lr = lr_model.best_model.predict_proba(lr_model.svd.transform(lr_model.vectorizer.transform(df_test['text'])))[:, 1]
-
-    roc_auc_nb = plot_roc_curve(y_test, y_proba_nb, "ROC Curve - Naive Bayes", os.path.join(output_dir, 'roc_curve_nb.png'))
-    roc_auc_svm = plot_roc_curve(y_test, y_proba_svm, "ROC Curve - SVM", os.path.join(output_dir, 'roc_curve_svm.png'))
-    roc_auc_lr = plot_roc_curve(y_test, y_proba_lr, "ROC Curve - Logistic Regression", os.path.join(output_dir, 'roc_curve_lr.png'))
+    y_proba_svm_50 = svm_model_50.best_model.predict_proba(svm_model_50.svd.transform(svm_model_50.vectorizer.transform(df_test['text'])))[:, 1]
+    y_proba_lr_50 = lr_model_50.best_model.predict_proba(lr_model_50.svd.transform(lr_model_50.vectorizer.transform(df_test['text'])))[:, 1]
+    y_proba_svm_100 = svm_model_100.best_model.predict_proba(svm_model_100.svd.transform(svm_model_100.vectorizer.transform(df_test['text'])))[:, 1]
+    y_proba_lr_100 = lr_model_100.best_model.predict_proba(lr_model_100.svd.transform(lr_model_100.vectorizer.transform(df_test['text'])))[:, 1]
 
     # Calculer et afficher les scores Gini
-    gini_nb = calculate_gini(roc_auc_nb)
-    gini_svm = calculate_gini(roc_auc_svm)
-    gini_lr = calculate_gini(roc_auc_lr)
+    gini_nb = calculate_gini(y_test, y_proba_nb)
+    gini_svm_50 = calculate_gini(y_test, y_proba_svm_50)
+    gini_lr_50 = calculate_gini(y_test, y_proba_lr_50)
+    gini_svm_100 = calculate_gini(y_test, y_proba_svm_100)
+    gini_lr_100 = calculate_gini(y_test, y_proba_lr_100)
 
     print(f"Score Gini pour Naive Bayes: {gini_nb:.2f}")
-    print(f"Score Gini pour SVM: {gini_svm:.2f}")
-    print(f"Score Gini pour Logistic Regression: {gini_lr:.2f}")
+    print(f"Score Gini pour SVM (50): {gini_svm_50:.2f}")
+    print(f"Score Gini pour Logistic Regression (50): {gini_lr_50:.2f}")
+    print(f"Score Gini pour SVM (100): {gini_svm_100:.2f}")
+    print(f"Score Gini pour Logistic Regression (100): {gini_lr_100:.2f}")
+
+    y_proba_dict = {
+        "Naive Bayes": y_proba_nb,
+        "SVM 50": y_proba_svm_50,
+        "Logistic Regression 50": y_proba_lr_50,
+        "SVM 100": y_proba_svm_100,
+        "Logistic Regression 100": y_proba_lr_100
+    }
+
+    # Générer et sauvegarder les courbes ROC AUC pour comparaison
+    plot_roc_curve_comparison(y_test, y_proba_dict, model_names=y_proba_dict.keys(), output_path=os.path.join(output_dir, 'roc_curve_comparison.png'))
+
 
     # Afficher les rapports de classification
     print("Rapport de classification pour Naive Bayes :")
     print(report_nb)
 
-    print("Rapport de classification pour SVM :")
-    print(report_svm)
+    print("Rapport de classification pour SVM 50 :")
+    print(report_svm_50)
 
-    print("Rapport de classification pour Logistic Regression :")
-    print(report_lr)
+    print("Rapport de classification pour Logistic Regression 50 :")
+    print(report_lr_50)
+
+    print("Rapport de classification pour SVM 100 :")
+    print(report_svm_100)
+
+    print("Rapport de classification pour Logistic Regression 100 :")
+    print(report_lr_100)
 
 if __name__ == "__main__":
     main()
--- src/visualisation/viz.py
+++ src/visualisation/viz.py
@@ -5,6 +5,8 @@ from langdetect import detect, DetectorFactory
 import nltk
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score, auc
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 nltk.download('punkt')
 
@@ -70,25 +72,35 @@ def plot_confusion_matrix(y_true, y_pred, title, output_path):
     plt.savefig(output_path)
     plt.show()
 
-def plot_roc_curve(y_true, y_proba, title, output_path):
-    """Trace la courbe ROC et sauvegarde l'image."""
+def plot_roc_curve_comparison(y_test, y_proba_dict, model_names, output_path):
+    """Trace les courbes ROC AUC pour plusieurs modèles sur un seul graphique interactif avec Plotly."""
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+
+    for model_name, y_proba in y_proba_dict.items():
+        fpr, tpr, _ = roc_curve(y_test, y_proba)
+        roc_auc = auc(fpr, tpr)
+
+        fig.add_trace(
+            go.Scatter(
+                x=fpr, y=tpr,
+                mode='lines',
+                name=f'{model_name} (area = {roc_auc:.2f})'
+            )
+        )
+
+    fig.update_layout(
+        title='ROC Curve Comparison',
+        xaxis_title='False Positive Rate',
+        yaxis_title='True Positive Rate',
+        legend_title="Models",
+    )
+    fig.update_yaxes(range=[0, 1])
+    fig.update_xaxes(range=[0, 1])
+    fig.write_image(output_path)
+    fig.show()
+
+def calculate_gini(y_true, y_proba):
+    """Calcule le score Gini à partir de l'AUC."""
     fpr, tpr, _ = roc_curve(y_true, y_proba)
     roc_auc = auc(fpr, tpr)
-
-    plt.figure()
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:0.2f})')
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title(title)
-    plt.legend(loc="lower right")
-    plt.savefig(output_path)
-    plt.show()
-
-    return roc_auc
-
-def calculate_gini(roc_auc):
-    """Calcule le score Gini à partir de l'AUC."""
     return 2 * roc_auc - 1
